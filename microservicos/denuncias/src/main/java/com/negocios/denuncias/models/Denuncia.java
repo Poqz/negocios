8df70e4e689544122b161d45dbcbc3ff532ea9b2
strangecousinwst <139879737+strangecousinwst@users.noreply.github.com>
2025-04-10 10:13:21 +0100

    +models

--- microservicos/denuncias/src/main/java/com/negocios/denuncias/models/Denuncia.java
+++ microservicos/denuncias/src/main/java/com/negocios/denuncias/models/Denuncia.java
@@ -1,4 +1,63 @@
 package com.negocios.denuncias.models;
 
+import java.util.Date;
+
 public class Denuncia {
+    private int id;
+    private String descricao;
+    private int denunciador_ID;
+    private int denunciado_ID;
+    private Date data;
+    private int tipoDenunciaId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getDenunciador_ID() {
+        return denunciador_ID;
+    }
+
+    public void setDenunciador_ID(int denunciador_ID) {
+        this.denunciador_ID = denunciador_ID;
+    }
+
+    public int getDenunciado_ID() {
+        return denunciado_ID;
+    }
+
+    public void setDenunciado_ID(int denunciado_ID) {
+        this.denunciado_ID = denunciado_ID;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public int getTipoDenunciaId() {
+        return tipoDenunciaId;
+    }
+
+    public void setTipoDenunciaId(int tipoDenunciaId) {
+        this.tipoDenunciaId = tipoDenunciaId;
+    }
 }
+
+
+
